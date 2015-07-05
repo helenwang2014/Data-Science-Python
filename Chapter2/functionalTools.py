d02a31b12bf5905999599459aa81860904b71327
helenwang2014 <helen.wang2013@gmail.com>
2015-07-05 16:26:10 -0400

    Data-Science-Python/

--- Chapter2/functionalTools.py
+++
@@ -1,6 +0,0 @@
-list1 = ['a', 'b', 'c']
-list2 = [1,2,3]
-
-zip(list1,list2)
-print zip()
-
--- Chapter2/sets.py
+++
@@ -1,9 +0,0 @@
-s = set()
-s.add(1)
-s.add(2)
-s.add(2)
-x = len(s)
-y = 2 in s
-z = 3 in s
-print x, y, z
-
