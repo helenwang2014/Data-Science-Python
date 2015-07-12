0ef5c95e757f2b59d861d9eec8bf4f6c89b869da
helenwang2014 <helen.wang2013@gmail.com>
2015-07-11 21:03:16 -0400

    statistics

--- Chapter5/dispersion.py
+++ Chapter5/dispersion.py
@@ -1,9 +1,9 @@
 import math
+import numpy
 
+num_friends = [100,49,41,40,25,34,4, 3, 2, 1, 1, 7,  7,  8,  8]
 
-num_friends = [100,49,41,40,25, 34,4,3,2,1,1,7,7,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8]
-
-
+daily_minutes =[60,34,3,42,23,36,15,47,49,45,23,32, 34, 67, 89 ]
 
 def data_range(x):
 	return max(x) - min(x)
@@ -24,14 +24,14 @@ def variance(x):
 	deviations = de_mean(x)
 	return sum(deviations) / (n - 1)
 
-print variance(num_friends)
+print "variance is: ", variance(num_friends)
 
 
 
 def standard_deviation(x):
-	return math.sqrt(x)
+	return float(math.sqrt(x))
 
-print standard_deviation(variance(num_friends))
+print "standard deviation is: ", standard_deviation(variance(num_friends))
 
 
 def quantile(x, p):
@@ -42,4 +42,24 @@ def interquartile_range(x):
 	return quantile(x, 0.75) - quantile(x, 0.25)
 
 print interquartile_range(num_friends)
+
+
+def covariance(x, y):
+	n = len(x)
+	return numpy.dot(de_mean(x), de_mean(y))/(n - 1)
+
+print "covariance is: ", covariance(num_friends, daily_minutes)
+
+
+def correlation(x, y):
+	stdev_x = standard_deviation(x)
+	stdev_y = standard_deviation(y)
+	if stdev_x >0 and stdev_y > 0:
+		return float(covariance(x, y)/stdev_x/stdev_y)
+	else:
+		return 0 
+print "correlation is: ", correlation(num_friends, daily_minutes)
+
+
+
 
