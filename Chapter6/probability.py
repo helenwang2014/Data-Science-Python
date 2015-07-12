4e8db83d8c82871d0b3cf3498182d3e445d8536c
helenwang2014 <helen.wang2013@gmail.com>
2015-07-11 23:23:13 -0400

    conditional_probability

--- Chapter6/probability.py
+++ Chapter6/probability.py
@@ -7,17 +7,18 @@ both_girls = 0.0
 older_girl = 0.0
 either_girl = 0.0
 
-random.seed()
+random.seed(0)
 for _ in range(100000):
 	younger = random_kid()
 	older = random_kid()
 	if older == "girl":
-		older_girl = older_girl + 1
+		older_girl += 1
 	if older == "girl" and younger == "girl":
-		both_girls = both_girls + 1
-	if older == "girl" or "younger" == "girl":
-		either_girl = either_girl + 1
+		both_girls += 1
+	if older == "girl" or younger == "girl":
+		either_girl += 1
 
-print "p(both|older):", both_girls/older_girl
-print "p(both|either):", both_girls/either_girl
+print "p(both|older):", round(both_girls/older_girl, 2)
+print "p(both|either):", round(both_girls/either_girl, 2)
+
 
