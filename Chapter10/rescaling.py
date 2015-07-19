2cd65e77abb3102d8edc18e845296d87dcbf506e
helenwang2014 <helen.wang2013@gmail.com>
2015-07-19 00:47:26 -0400

    rescaling

--- Chapter10/rescaling.py
+++ Chapter10/rescaling.py
@@ -1,5 +1,6 @@
 import math
 from scipy import spatial
+from numpy import dot
 
 a_to_b = spatial.distance.euclidean([63, 150], [67, 160])
 a_to_c = spatial.distance.euclidean([63, 150], [70, 160])
@@ -15,7 +16,37 @@ def make_matrix(num_rows, num_cols, entry_fn):
     whose (i,j)-th entry is entry_fn(i, j)"""
     return [[entry_fn(i, j) for j in range(num_cols)]
             for i in range(num_rows)]  
+def mean(x):
+	return sum(x) / len(x)
 
+def sum_of_squares(v):
+    """v_1 * v_1 + ... + v_n * v_n"""
+    return dot(v, v)
+
+def de_mean(x):
+    """translate x by subtracting its mean (so the result has mean 0)"""
+    x_bar = mean(x)
+    return [x_i - x_bar for x_i in x]
+
+def variance(x):
+    """assumes x has at least two elements"""
+    n = len(x)
+    deviations = de_mean(x)
+    return sum_of_squares(deviations) / (n - 1)
+    
+def standard_deviation(x):
+    return math.sqrt(variance(x))
+
+def get_row(A, i):
+    return A[i]
+    
+def get_column(A, j):
+    return [A_i[j] for A_i in A]
+
+def shape(A):
+    num_rows = len(A)
+    num_cols = len(A[0]) if A else 0
+    return num_rows, num_cols
 
 def scale(data_matrix):
 	# returns the means and standard deviation of each column
@@ -43,4 +74,7 @@ def rescale(data_matrix):
 	num_rows, num_cols = shape(data_matrix)
 	return make_matrix(num_rows, num_cols, rescaled)
 
-	
+
+print scale([[63,23],[3,4], [87,35], [43,34], [32, 14]])
+
+
